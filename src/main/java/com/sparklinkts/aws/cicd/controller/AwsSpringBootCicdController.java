5239e3990707df58fafbadc6fde42ecdbd038e84
sparklink-ts <sparklink.ts@gmail.com>
2026-02-16 21:21:36 +0530

    Updated with SNS and Prod Deployment

--- src/main/java/com/sparklinkts/aws/cicd/controller/AwsSpringBootCicdController.java
+++ src/main/java/com/sparklinkts/aws/cicd/controller/AwsSpringBootCicdController.java
@@ -8,7 +8,7 @@ public class AwsSpringBootCicdController {
 
     @GetMapping("/aws-cicd")
     public String getMessage(){
-        return "AWS - CICD with Elastic Beanstalk - Code Pipeline - Code Build is done !!! Great Work !!!";
+        return "AWS - CICD with Elastic Beanstalk (Staging Server : Stage Deployment ) - Code Pipeline - Code Build - SNS (POD Lead Approval) and Elastic Beanstalk (Production Server : Prod Deployment ) is Working Fine !!! Great Work !!!";
     }
 
 }
